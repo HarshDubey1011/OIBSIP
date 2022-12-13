68e8cc6d50d36472f1af0324a372db7cb07481b4
harsushka <pandethallu@gmail.com>
2022-12-13 19:18:28 +0530

    Changes on components has been commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation'
+import About from './components/About/About'
+import Header from './components/Header/Header'
+import Contact from './components/Contact/Contact'
+import Project from './components/Project/Project'
+import Footer from './components/Footer/Footer'
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navigation />
+        <Header />
+        <About />
+        <Project />
+        <Contact />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <div className='container'>
+        <nav>
+            <img src='' alt='Not Available' className='logo'/>
+            <ul className='navbar'>
+                <li><a href='' className='nav-anchor'>Home</a></li>
+                <li><a href='' className='nav-anchor'>About</a></li>
+                <li><a href='' className='nav-anchor'>Projects</a></li>
+                <li><a href='' className='nav-anchor'>Contact</a></li>
+            </ul>
+        </nav>  
+    </div>
+  )
+}
